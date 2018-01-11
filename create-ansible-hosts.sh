1104dc1e2178173e28b3f60c5d512fa4825193b3
mariojmdavid <mariojmdavid@gmail.com>
2018-01-11 14:20:22 +0000

    add echo in script

--- create-ansible-hosts.sh
+++ create-ansible-hosts.sh
@@ -8,8 +8,12 @@
 
 IP_FILE=/tmp/ippub.txt
 
+echo "Gathering all Public IPs"
 openstack floating ip list -f value -c 'Floating IP Address' > ${IP_FILE}
 
-
-
+echo "Printing all lines in the file - Public IPs"
+while IFS= read -r var
+do
+  echo "$var"
+done < "${IP_FILE}"
 
