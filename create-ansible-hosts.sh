22c9b245626fbb618b92bf82087668d0104bd0a6
mariojmdavid <mariojmdavid@gmail.com>
2018-01-11 15:08:47 +0000

    add creation of ansible hosts file

--- create-ansible-hosts.sh
+++ create-ansible-hosts.sh
@@ -7,13 +7,29 @@
 #
 
 IP_FILE=/tmp/ippub.txt
+ANS_HOST=/tmp/hosts
+
+rm -f ${ANS_HOST}
 
 echo "Gathering all Public IPs"
-openstack floating ip list -f value -c 'Floating IP Address' > ${IP_FILE}
+
+if [ ! -f ${IP_FILE} ]
+  openstack floating ip list -f value -c 'Floating IP Address' > ${IP_FILE}
+fi
 
 echo "Printing all lines in the file - Public IPs"
 while IFS= read -r var
 do
-  echo "$var"
+  echo "---> $var"
+  if ssh -o ConnectTimeout=3 -o StrictHostKeyChecking=no -o BatchMode=yes ubuntu@${var} "uname -a"
+  then
+    echo "${var} ansible_connection=ssh ansible_user=ubuntu ansible_become=true" >> ${ANS_HOST}
+  fi
+  
+  if ssh -o ConnectTimeout=3 -o StrictHostKeyChecking=no -o BatchMode=yes centos@${var} "uname -a"
+  then
+    echo "${var} ansible_connection=ssh ansible_user=centos ansible_become=true" >> ${ANS_HOST}
+  fi
+
 done < "${IP_FILE}"
 
