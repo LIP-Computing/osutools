ba5086241038171fb0a70b539dd4b69f96348c63
mariojmdavid <mariojmdavid@gmail.com>
2018-01-11 16:35:02 +0000

    fixes to script

--- create-ansible-hosts.sh
+++ create-ansible-hosts.sh
@@ -7,30 +7,43 @@
 #
 
 IP_FILE=/tmp/ippub.txt
+TMP_HOSTS=/tmp/hosts-tmp
 ANS_HOST=/tmp/hosts
 
-rm -f ${ANS_HOST}
+rm -f ${TMP_HOSTS} ${ANS_HOST}
 
 echo "Gathering all Public IPs"
 
 if [ ! -f ${IP_FILE} ]
 then
+  echo "--- Creating file with public IPs"
   openstack floating ip list -f value -c 'Floating IP Address' > ${IP_FILE}
 fi
 
-echo "Printing all lines in the file - Public IPs"
-while IFS= read -r var
+echo "Printing all lines in the file - Public IPs file ${IP_FILE}"
+for var in $(cat ${IP_FILE})
 do
-  echo "---> $var"
-  if ssh -o ConnectTimeout=3 -o StrictHostKeyChecking=no -o BatchMode=yes ubuntu@${var} "uname -a"
+  ssh -o ConnectTimeout=3 -o StrictHostKeyChecking=no -o BatchMode=yes ubuntu@${var} "uname -a"
+  retval="$?"
+  echo "---> $var ubuntu -- $retval"
+  if [ $retval -eq 0 ]
   then
-    echo "${var} ansible_connection=ssh ansible_user=ubuntu ansible_become=true" >> ${ANS_HOST}
+    echo "${var} ansible_connection=ssh ansible_user=ubuntu ansible_become=true" >> ${TMP_HOSTS}
   fi
   
-  if ssh -o ConnectTimeout=3 -o StrictHostKeyChecking=no -o BatchMode=yes centos@${var} "uname -a"
+  ssh -o ConnectTimeout=3 -o StrictHostKeyChecking=no -o BatchMode=yes centos@${var} "uname -a"
+  retval="$?"
+  echo "---> $var centos -- $retval"
+  if [ $retval -eq 0 ]
   then
-    echo "${var} ansible_connection=ssh ansible_user=centos ansible_become=true" >> ${ANS_HOST}
+    echo "${var} ansible_connection=ssh ansible_user=centos ansible_become=true" >> ${TMP_HOSTS}
   fi
 
-done < "${IP_FILE}"
+done
+
+echo "[ubuntu]" >> ${ANS_HOST}
+grep ubuntu ${TMP_HOSTS} >> ${ANS_HOST}
+echo "[centos]" >> ${ANS_HOST}
+grep centos ${TMP_HOSTS} >> ${ANS_HOST}
+
 
