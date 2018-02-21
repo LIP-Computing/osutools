347d1b4013914a6ebf9ebbebea012c69e1c56c5d
mariojmdavid <mariojmdavid@gmail.com>
2018-02-21 15:42:58 +0000

    add docker volume options for nvidia libs

--- get-nvidialibs.sh
+++ get-nvidialibs.sh
@@ -9,6 +9,12 @@
 OS_TYPE=`grep "^ID_LIKE=" /etc/os-release|cut -d'=' -f 2`
 BIN_PREFIX="/usr/bin/"
 
+#OPT is the docker volume option
+for vol in "/etc/vulkan/icd.d/nvidia_icd.json" "/etc/OpenCL/vendors/nvidia.icd"
+do
+  OPT="${OPT} -v ${vol}:${vol}"
+done
+
 if [ "${OS_TYPE}" = "debian" ]
 then
   LIB64_PREFIX="/usr/lib/x86_64-linux-gnu/"
@@ -28,6 +34,30 @@ do
   OPT="${OPT} -v ${vol}:${vol}"
 done
 
-echo ${OPT}
+for vol in `find ${LIB64_PREFIX} -name 'lib*GL*'|grep -v mesa|grep -v GLU`
+do
+  OPT="${OPT} -v ${vol}:${vol}"
+done
 
+for vol in `find ${LIB64_PREFIX} -name 'libOpenCL*'`
+do
+  OPT="${OPT} -v ${vol}:${vol}"
+done
+
+for vol in `find ${LIB64_PREFIX} -name 'libcuda*'`
+do
+  OPT="${OPT} -v ${vol}:${vol}"
+done
+
+for vol in `find ${LIB64_PREFIX} -name 'libvdpau*'`
+do
+  OPT="${OPT} -v ${vol}:${vol}"
+done
+
+for vol in `find ${LIB64_PREFIX} -name 'libnv*'|grep -v xorg|grep -v container`
+do
+  OPT="${OPT} -v ${vol}:${vol}"
+done
+
+echo ${OPT}
 
