9bf29e2571f36ca5ba6f2ea9ac6235e881284985
mariojmdavid <mariojmdavid@gmail.com>
2018-02-21 13:16:23 +0000

    add get nvidia libs and bin

---
+++ get-nvidialibs.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#
+# Copyright 2017 LIP
+#
+# Author: Mario David <mariojmdavid@gmail.com>
+#
+
+OS_TYPE=`grep "^ID_LIKE=" /etc/os-release|cut -d'=' -f 2`
+BIN_PREFIX="/usr/bin/"
+
+if [ ${OS_TYPE} -eq 'debian' ]
+then
+  LIB64_PREFIX="/usr/lib/x86_64-linux-gnu/"
+fi
+
+if [[ ${OS_TYPE} =~ *rhel* ]]
+then
+  LIB64_PREFIX="/usr/lib64/"
+fi
+
+echo "LIB64_PREFIX= ${LIB64_PREFIX}"
+
