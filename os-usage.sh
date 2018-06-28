f8df5048abc5ffc2e6c82a1a950eb65be19fc8d5
mariojmdavid <mariojmdavid@gmail.com>
2018-06-28 14:33:43 +0100

    fix os usage

--- os-usage.sh
+++ os-usage.sh
@@ -4,7 +4,7 @@
 while IFS=' ' read -r start end
 do
     echo "openstack usage list --start '$start' --end '$end' -f csv -c Project -c Servers"
-    openstack usage list --start '$start' --end '$end' -f csv -c Project -c Servers > servers-$start.csv
-    openstack usage list --start '$start' --end '$end' -f csv -c Project -c "CPU Hours" > cpus-$start.csv
+    openstack usage list --start "$start" --end "$end" -f csv -c Project -c Servers > servers-$start.csv
+    openstack usage list --start "$start" --end "$end" -f csv -c Project -c "CPU Hours" > cpus-$start.csv
 done < start-end.txt
 
