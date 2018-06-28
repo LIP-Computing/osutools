a11b1f9d57f74656525a99d82b87d8472c88184f
mariojmdavid <mariojmdavid@gmail.com>
2018-06-28 15:32:28 +0100

    fix year month

--- usage-analisys.py
+++ usage-analisys.py
@@ -5,12 +5,13 @@ with open('year-month-nimbus.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
         dstart = str(row[0]) + '-' + str(row[1]) + '-' + str(row[2])
+        year_month = str(row[0]) + '-' + str(row[1])
         #print "server-" + dstart + ".csv"
         data_csv = "servers-" + dstart + ".csv"
 
         with open(data_csv) as dcsv:
             rcsv = csv.reader(dcsv, delimiter=',')
             for l in rcsv:
-                print l[0], dstart, l[1]
+                print l[0], year_month, l[1]
         
 
