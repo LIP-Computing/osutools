2ab726bac7c5dda247aa3f8fe69866a0a81dd46e
mariojmdavid <mariojmdavid@gmail.com>
2018-06-28 14:25:31 +0100

    change scripts for os usage

--- os-usage.sh
+++ os-usage.sh
@@ -3,6 +3,8 @@
 
 while IFS=' ' read -r start end
 do
-    echo "openstack usage list --start '$start' --end '$end'"
+    echo "openstack usage list --start '$start' --end '$end' -f csv -c Project -c Servers"
+    openstack usage list --start '$start' --end '$end' -f csv -c Project -c Servers > servers-$start.csv
+    openstack usage list --start '$start' --end '$end' -f csv -c Project -c "CPU Hours" > cpus-$start.csv
 done < start-end.txt
 
