82684fe8a6a36aa2d17340b2b9851b00bbc7d3b9
mariojmdavid <mariojmdavid@gmail.com>
2018-06-28 14:55:56 +0100

    analisys python script

---
+++ usage-analisys.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import csv
+
+with open('year-month-nimbus.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        dstart = str(row[0]) + '-' + str(row[1]) + '-' + str(row[2])
+        #print "server-" + dstart + ".csv"
+        data_csv = "server-" + dstart + ".csv"
+
+        with open(data_csv) as dcsv:
+            rcsv = csv.reader(dcsv, delimiter=',')
+            for l in rcsv:
+                print l[0], dstart, l[1]
+        
+
