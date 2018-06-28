5e2bd3fca7543577bc19db5242c18dd6b0036a8e
mariojmdavid <mariojmdavid@gmail.com>
2018-06-28 16:48:01 +0100

    good pandas

--- usage-analisys.py
+++ usage-analisys.py
@@ -3,14 +3,12 @@ import csv
 import pprint
 import pandas as pd
 
-projs = list()
 tbl = dict()
 with open('year-month-nimbus.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
         dstart = str(row[0]) + '-' + str(row[1]) + '-' + str(row[2])
         year_month = str(row[0]) + '-' + str(row[1])
-        #print "server-" + dstart + ".csv"
         data_csv = "servers-" + dstart + ".csv"
         tbl[year_month] = dict()
 
@@ -22,21 +20,30 @@ with open('year-month-nimbus.csv') as csvfile:
                 print l[0], year_month, l[1]
                 tbl[year_month][l[0]] = l[1]
 
-                if l[0] not in projs:
-                    projs.append(l[0])
-
-print
-#print projs
-#print        
-#pprint.pprint(tbl)
 df = pd.DataFrame(tbl)
 
-df.to_csv('os-table.csv')
+df.to_csv('os-table-instances.csv')
 
-#with open('os-table.csv', 'w') as csvf:
-#    r1 = ["Projects"]
-#    for r in tbl:
-#        r1.append(r)
-#    row1 = ','.join(r1)
-#    csvf.write(row1)
+
+tbl = dict()
+with open('year-month-nimbus.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        dstart = str(row[0]) + '-' + str(row[1]) + '-' + str(row[2])
+        year_month = str(row[0]) + '-' + str(row[1])
+        data_csv = "cpus-" + dstart + ".csv"
+        tbl[year_month] = dict()
+
+        with open(data_csv) as dcsv:
+            rcsv = csv.reader(dcsv, delimiter=',')
+            for l in rcsv:
+                if l[0] == 'Project':
+                    continue
+                print l[0], year_month, l[1]
+                tbl[year_month][l[0]] = l[1]
+
+df = pd.DataFrame(tbl)
+
+df.to_csv('os-table-cpus.csv')
+
 
